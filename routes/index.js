611d4cf36c74f16e44be74a60f18536b4c6cdc5b
Tony Yeung <mr.tyeung@gmail.com>
2013-05-04 16:32:48 -0700

    Added prototypes of home and 'create or rate' pages

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,9 @@ exports.index = function(req, res){
       company = 'Pitch Refiner';
   res.render('index', { title: company, tm: company + ' ' + tm.getFullYear() })
 };
+
+exports.createOrRate = function(req, res){
+  var tm = new Date(),
+      company = 'Pitch Refiner';
+  res.render('create_or_rate', { title: company, tm: company + ' ' + tm.getFullYear() })
+};
