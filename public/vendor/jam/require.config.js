95638b05a510a1bb6def554b72bb8d6c29461112
Tony Yeung <mr.tyeung@gmail.com>
2013-05-04 15:11:20 -0700

    Added bootstrap-amd, require-jade, and jquery-mobile

--- public/vendor/jam/require.config.js
+++ public/vendor/jam/require.config.js
@@ -11,6 +11,10 @@ var jam = {
             "main": "backbone.layoutmanager.js"
         },
         {
+            "name": "bootstrap-amd",
+            "location": "../vendor/jam/bootstrap-amd"
+        },
+        {
             "name": "jquery",
             "location": "../vendor/jam/jquery",
             "main": "jquery.js"
@@ -19,9 +23,14 @@ var jam = {
             "name": "lodash",
             "location": "../vendor/jam/lodash",
             "main": "./lodash.js"
+        },
+        {
+            "name": "require-jade",
+            "location": "../vendor/jam/require-jade",
+            "main": "jade.js"
         }
     ],
-    "version": "0.2.11",
+    "version": "0.2.12",
     "shim": {
         "backbone": {
             "deps": [
@@ -42,10 +51,108 @@ var jam = {
 };
 
 if (typeof require !== "undefined" && require.config) {
-    require.config({packages: jam.packages, shim: jam.shim});
+    require.config({
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "bootstrap-amd",
+            "location": "../vendor/jam/bootstrap-amd"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "require-jade",
+            "location": "../vendor/jam/require-jade",
+            "main": "jade.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        }
+    }
+});
 }
 else {
-    var require = {packages: jam.packages, shim: jam.shim};
+    var require = {
+    "packages": [
+        {
+            "name": "backbone",
+            "location": "../vendor/jam/backbone",
+            "main": "backbone.js"
+        },
+        {
+            "name": "backbone.layoutmanager",
+            "location": "../vendor/jam/backbone.layoutmanager",
+            "main": "backbone.layoutmanager.js"
+        },
+        {
+            "name": "bootstrap-amd",
+            "location": "../vendor/jam/bootstrap-amd"
+        },
+        {
+            "name": "jquery",
+            "location": "../vendor/jam/jquery",
+            "main": "jquery.js"
+        },
+        {
+            "name": "lodash",
+            "location": "../vendor/jam/lodash",
+            "main": "./lodash.js"
+        },
+        {
+            "name": "require-jade",
+            "location": "../vendor/jam/require-jade",
+            "main": "jade.js"
+        }
+    ],
+    "shim": {
+        "backbone": {
+            "deps": [
+                "jquery",
+                "lodash"
+            ],
+            "exports": "Backbone"
+        },
+        "backbone.layoutmanager": {
+            "deps": [
+                "jquery",
+                "backbone",
+                "lodash"
+            ],
+            "exports": "Backbone.LayoutManager"
+        }
+    }
+};
 }
 
 if (typeof exports !== "undefined" && typeof module !== "undefined") {
