8d3b03acd0a0c5e758c84a468c45811755c0e799
Tony Yeung <mr.tyeung@gmail.com>
2013-05-05 01:17:43 -0700

    Added fb login

---
+++ public/js/firebaseAuth.js
@@ -0,0 +1,24 @@
+(function($, win) {
+  var doc = window.document;
+
+  $(doc).ready(function () {
+    var ref = new Firebase('https://pitchrefiner.firebaseio.com');
+
+    var authClient = new FirebaseAuthClient(ref, function(error, user) {
+      if (error) {
+        // an error occurred while attempting login
+        console.log(error);
+      } else if (user) {
+        // user authenticated with Firebase
+        console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+      } else {
+        // user is logged out
+      }
+    });
+
+    $('#facebook_login').click(function (evt) {
+      console.log('facebook login button clicked');
+      authClient.login('facebook');
+    });
+  });
+})(jQuery, window);
