de61581cee39ffde1b59d8c65d898341c97f3fc9
Tony Yeung <mr.tyeung@gmail.com>
2013-05-04 17:08:20 -0700

    Added new prototype pages for create/rate

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/create_or_rate', routes.createOrRate);
+app.get('/create', routes.create);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -14,3 +14,9 @@ exports.createOrRate = function(req, res){
       company = 'Pitch Refiner';
   res.render('create_or_rate', { title: company, tm: company + ' ' + tm.getFullYear() })
 };
+
+exports.create = function(req, res){
+  var tm = new Date(),
+      company = 'Pitch Refiner';
+  res.render('create', { title: company, tm: company + ' ' + tm.getFullYear() })
+};
